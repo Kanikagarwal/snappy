9d29b4a672085963e25fd317990382e3b8c442a9
Kanika Agarwal <93864492+Kanikagarwal@users.noreply.github.com>
2025-07-14 21:03:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://snappy-chatapp-five.vercel.app",
+    credentials: true, // if you're using cookies or auth headers
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth",userRoutes)
