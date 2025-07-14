2dc47cb044d33ae7bc3e61e826639615e89a3681
Kanika Agarwal <93864492+Kanikagarwal@users.noreply.github.com>
2025-07-14 20:11:44 +0530

    first commit

--- frontend/src/APIRoutes.js
+++ frontend/src/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = import.meta.env.HOST;
 // const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const registerRoute = `${host}/api/auth/register`;
