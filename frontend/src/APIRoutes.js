1292d19d15f87ada74df70b3565644c1e75bdc5c
Kanika Agarwal <93864492+Kanikagarwal@users.noreply.github.com>
2025-07-14 20:58:31 +0530

    Update APIRoutes.js

--- frontend/src/APIRoutes.js
+++ frontend/src/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = import.meta.env.HOST;
+export const host = "https://snappy-bice-three.vercel.app/";
 // const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const registerRoute = `${host}/api/auth/register`;
